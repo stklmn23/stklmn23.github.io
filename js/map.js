104048d0a11913f8c0a6ddcfde15f15df80749b1
Stanislav <stanislav_chudin@mail.ru>
2023-04-07 20:08:55 +0500

    Add layers with cells and routes

--- js/map.js
+++ js/map.js
@@ -26,24 +26,61 @@ var StopIcon = L.icon({
 });
 
 const markers = L.markerClusterGroup();
+
 var stops_layer = L.geoJSON(all_stops, {
-    onEachFeature: function (feature, layer) {
-        var popupTxt = 'Наименование: ' + feature.properties.stop_name + '<br>' +
-        'Вид транспорта: ' + feature.properties.type
-        layer.bindPopup(popupTxt);
-    },
     pointToLayer: function (feature, latlng){
-        return markers.addLayer(L.marker(latlng, {icon: StopIcon}))
-    },
+        return markers.addLayer(L.marker(latlng, {icon: StopIcon}).bindPopup(
+            'Наименование: ' + feature.properties.stop_name + '<br>' +
+            'Вид транспорта: ' + feature.properties.type)
+    )},
 });
+
+// Линии маршрутов (чтение из GeoJSON)
+// var city_boundary_style = {
+//     "color": '#FF94C5',
+//     "fill": false,
+//     "weight": 5,
+//     "dashArray": [10,  10]
+// };
+var routes_layer = L.geoJSON(all_routes, {
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup(
+        '№: ' + feature.properties.route_name + '<br>' +
+        'Вид транспорта: ' + feature.properties.type + '<br>' +
+        'Трасса: ' + feature.properties.trace)
+    },
+  });
+
+// Ячейки города pop/work (функции отображения + чтение из GeoJSON)
+function getColor(d) {
+    return d > 1000 ? '#800026' :
+           d > 500  ? '#BD0026' :
+           d > 200  ? '#E31A1C' :
+           d > 100  ? '#FC4E2A' :
+           d > 10   ? '#FED976' :
+                      '#FFEDA0';
+}
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.pop),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.5
+    };
+}
+var cells_layer = L.geoJSON(cells, {style: style});
 
 // ОБЪЯВЛЕНИЕ ГРУПП СЛОЁВ
 var boundaries = L.layerGroup([city_boundary_layer]);
 var stops = L.layerGroup([stops_layer]);
+var routes = L.layerGroup([routes_layer]);
+var cells = L.layerGroup([cells_layer]);
 
 // Инициализация карты с центром и нужным масштабом и слоями
 let config = { minZoom: 11, maxZoom: 18, zoomControl: false, fullscreenControl: true,
-    layers: [osm, boundaries, stops]
+    layers: [osm, cells, boundaries, routes, stops]
 };
 var map = L.map('map', config).setView([55.09, 38.76], 13);
 
@@ -57,8 +94,10 @@ var baseMaps = {
 };
 
 var overlayMaps = {
-    "Границы города": boundaries,
     "Остановочные пункты": stops,
+    "Маршруты": routes,
+    "Население": cells_layer,
+    "Границы города": boundaries,
 };
 
 // Добавление слоёв на карту
