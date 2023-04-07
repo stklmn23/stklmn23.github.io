aa63cc4fd1c01e866670d45d23f6322709a4189c
Stanislav <stanislav_chudin@mail.ru>
2023-04-07 18:51:09 +0500

    Add stops clustering and icons

--- js/map.js
+++ js/map.js
@@ -1,15 +1,14 @@
 // Инициализация базовых карт
 var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Карта маршрутов Коломны <a href="https://yandex.com/maps">Test link</a>',
-    maxZoom: 18, minZoom: 10
 });
 
 var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 });
 
-// Объявление стилей и слоёв, чтение из GeoJSON файлов
-
+// ОБЪЯВЛЕНИЕ СТИЛЕЙ И СЛОЁВ
+// Граница города (стиль + чтение из GeoJSON)
 var city_boundary_style = {
     "color": '#FF94C5',
     "fill": false,
@@ -18,25 +17,35 @@ var city_boundary_style = {
 };
 var city_boundary_layer = L.geoJSON(city_boundary, {style: city_boundary_style});
 
+// Уникальные остановки (стиль + чтение из GeoJSON и кластеризация точек)
+var StopIcon = L.icon({
+    iconSize: [20, 20],
+    iconAnchor: [13, 27],
+    popupAnchor:  [1, -24],
+    iconUrl: 'icons/bus.png' 
+});
+
+const markers = L.markerClusterGroup();
 var stops_layer = L.geoJSON(all_stops, {
     onEachFeature: function (feature, layer) {
-      var popupTxt = 'Наименование: ' + feature.properties.stop_name + '<br>' +
-                     'Вид транспорта: ' + feature.properties.type
-      layer.bindPopup(popupTxt);
-    }      
-  });
+        var popupTxt = 'Наименование: ' + feature.properties.stop_name + '<br>' +
+        'Вид транспорта: ' + feature.properties.type
+        layer.bindPopup(popupTxt);
+    },
+    pointToLayer: function (feature, latlng){
+        return markers.addLayer(L.marker(latlng, {icon: StopIcon}))
+    },
+});
 
-// Объявление групп слоёв
+// ОБЪЯВЛЕНИЕ ГРУПП СЛОЁВ
 var boundaries = L.layerGroup([city_boundary_layer]);
 var stops = L.layerGroup([stops_layer]);
 
-// Инициализация карта с центром и нужным масштабом и слоями
-var map = L.map('map', {
-    center: [55.09, 38.76],
-    zoom: 13,
-    zoomControl: false,
-    layers: [osm, boundaries]
-});
+// Инициализация карты с центром и нужным масштабом и слоями
+let config = { minZoom: 11, maxZoom: 18, zoomControl: false, fullscreenControl: true,
+    layers: [osm, boundaries, stops]
+};
+var map = L.map('map', config).setView([55.09, 38.76], 13);
 
 // Размещение +/- масштаба
 L.control.zoom({ position: "bottomright" }).addTo(map);
@@ -49,13 +58,8 @@ var baseMaps = {
 
 var overlayMaps = {
     "Границы города": boundaries,
-    "Остановочные пункты": stops
+    "Остановочные пункты": stops,
 };
 
 // Добавление слоёв на карту
 var layerControl = L.control.layers(baseMaps, overlayMaps, {collapsed:false,}).addTo(map);
-
-// // Кластеризация точек остановок
-// var markers = L.markerClusterGroup();
-// markers.addLayer(stops_layer);
-// map.addLayer(markers);
