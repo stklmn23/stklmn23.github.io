9b6586a32427f3f77950c2127b2b450a48dcc015
Stanislav <stanislav_chudin@mail.ru>
2023-04-16 19:58:07 +0500

    Add control instruments

--- js/map.js
+++ js/map.js
@@ -102,3 +102,20 @@ var overlayMaps = {
 
 // Добавление слоёв на карту
 var layerControl = L.control.layers(baseMaps, overlayMaps, {collapsed:false,}).addTo(map);
+
+// FeatureGroup is to store editable layers
+var drawnItems = new L.FeatureGroup();
+map.addLayer(drawnItems);
+
+var drawControl = new L.Control.Draw({
+    edit: {
+        featureGroup: drawnItems
+    }
+});
+map.addControl(drawControl);
+
+map.on('draw:created', function (e) {
+    var type = e.layerType,
+        layer = e.layer;
+    drawnItems.addLayer(layer);
+});
